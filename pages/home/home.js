9c534a1cca5d8fe86c591f2672fc640637cd4f6d
weilai5201314 <w200111040833@163.com>
2024-05-09 18:48:40 +0800

    feat(projects): 制定系统框架结构

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,24 +1,40 @@
-// pages/home/home.js
-Component({
-
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
+Page({
+  mixins: [require('../mixin/common')],
   data: {
-
+    inputShowed: false,
+    inputVal: '',
+    isFocus: false,
+  },
+  showInput() {
+    this.setData({
+      inputShowed: true,
+    });
+  },
+  blurInput() {
+    this.setData({
+      isFocus: false,
+    });
+  },
+  hideInput() {
+    this.setData({
+      inputVal: '',
+      inputShowed: false,
+    });
+  },
+  clearInput() {
+    this.setData({
+      inputVal: '',
+    });
+  },
+  inputTyping(e) {
+    this.setData({
+      inputVal: e.detail.value,
+      isFocus: true,
+    });
   },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
-})
+  search() {
+    // 在这里编写搜索逻辑，可以根据 inputVal 的值执行相应的搜索操作
+    console.log('执行搜索：', this.data.inputVal);
+  },
+});
