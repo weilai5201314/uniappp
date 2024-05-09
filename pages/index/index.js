aaa58154d2c657db5217e6a3bad12ee0e4e9c64d
weilai5201314 <w200111040833@163.com>
2024-05-09 11:59:13 +0800

    feat(projects): 初步完善主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,8 @@
 // index.js
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0';
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {
       avatarUrl: defaultAvatarUrl,
       nickName: '',
@@ -12,38 +11,55 @@ Page({
     canIUseGetUserProfile: wx.canIUse('getUserProfile'),
     canIUseNicknameComp: wx.canIUse('input.type.nickname'),
   },
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   onChooseAvatar(e) {
-    const { avatarUrl } = e.detail
-    const { nickName } = this.data.userInfo
+    const {
+      avatarUrl
+    } = e.detail
+    const {
+      nickName
+    } = this.data.userInfo
     this.setData({
       "userInfo.avatarUrl": avatarUrl,
       hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
     })
   },
-  onInputChange(e) {
-    const nickName = e.detail.value
-    const { avatarUrl } = this.data.userInfo
-    this.setData({
-      "userInfo.nickName": nickName,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+  // 添加登录按钮点击事件处理函数
+  onLogin() {
+    // 调用 wx.login 获取用户登录凭证 code
+    wx.login({
       success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+        if (res.code) {
+          const code = res.code;
+          console.log("登录凭证code是(0a3rUl):" + code);
+          // 获取用户信息
+          // 将 code 和用户信息发送到后端服务器进行验证
+          wx.request({
+            url: 'http://localhost:12345/login',
+            method: 'POST',
+            data: {
+              code: code,
+            },
+            success: (res) => {
+              console.log('登录成功：', res.data);
+              // 可以在这里处理登录成功后的逻辑
+              wx.switchTab({
+                url: '/pages/home/home'
+              });
+            },
+            fail: (err) => {
+              console.error('登录失败：', err);
+            }
+          });
+        } else {
+          console.error('获取用户登录凭证失败：', res.errMsg);
+        }
+      },
+      fail: (err) => {
+        console.error('wx.login 调用失败：', err);
+      },
+      complete: (e) => {
+        console.log('wx.login 调用结束');
       }
-    })
+    });
   },
-})
+});
