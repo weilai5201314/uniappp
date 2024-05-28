d00fd450cc87a25b1df6b17c1e1c3f3591111375
weilai5201314 <w200111040833@163.com>
2024-05-28 18:28:22 +0800

    feat(projects): 修改购物车页面

--- pages/shoppingcart/shoppingcart.js
+++ pages/shoppingcart/shoppingcart.js
@@ -1,24 +1,88 @@
-// pages/shoppingcart/shoppingcart.js
-Component({
+const config = require("../../config")
 
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
+Page({
   data: {
-
+    inputShowed: false,
+    inputVal: '',
+    isFocus: false,
+    cartItems: [] // 用于存放购物车数据
   },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  onLoad() {
+    this.fetchCartItems();
+  },
+  onShow() {
+    this.fetchCartItems();
+  },
+  showInput() {
+    this.setData({
+      inputShowed: true,
+    });
+  },
+  blurInput() {
+    this.setData({
+      isFocus: false,
+    });
+  },
+  hideInput() {
+    this.setData({
+      inputVal: '',
+      inputShowed: false,
+    });
+  },
+  clearInput() {
+    this.setData({
+      inputVal: '',
+    });
+  },
+  inputTyping(e) {
+    this.setData({
+      inputVal: e.detail.value,
+      isFocus: true,
+    });
+  },
+  search() {
+    // 在这里编写搜索逻辑，可以根据 inputVal 的值执行相应的搜索操作
+    console.log('执行搜索：', this.data.inputVal);
+  },
+  fetchCartItems() {
+    const openid = wx.getStorageSync('openid');
+    wx.request({
+      url: config.baseUrl + '/getShoppingCart',
+      method: 'POST',
+      data: {
+        openid: openid
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: (res) => {
+        if (res.statusCode === 200) {
+          console.log(res)
+          this.setData({
+            cartItems: res.data.cartItems
+          });
+        } else {
+          wx.showToast({
+            title: '获取购物车失败',
+            icon: 'none',
+            duration: 2000
+          });
+        }
+      },
+      fail: (err) => {
+        console.error('请求失败：', err);
+        wx.showToast({
+          title: '获取购物车失败',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+  },
+  goToDetail(e) {
+    const product = e.currentTarget.dataset.product;
+    wx.navigateTo({
+      url: `/pages/productInfo/productInfo?product=${JSON.stringify(product)}`
+    });
   }
-})
+});
